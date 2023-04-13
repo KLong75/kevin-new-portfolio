873258cee64ad303b7040a589545800831663ade
Kevin Long <98487770+KLong75@users.noreply.github.com>
2023-04-13 08:16:55 -0500

    fix schedule link

--- src/components/ScheduleAppointment/index.js
+++ src/components/ScheduleAppointment/index.js
@@ -8,23 +8,20 @@ const ScheduleAppointment = () => {
   
   return (
     <Grid container spacing={1} justifyContent='center' textAlign='center' >
-    {/* <section > */}
       <h4 className='permanent-marker' style={{ marginTop:'0px'}}>&lt; Schedule an Appointment /&gt;</h4>
         <Grid xs={12} >
         <a 
-          href='https://calendly.com/' 
+          href='https://calendly.com/kevin_long/30min' 
           target='_blank' 
           rel='noreferrer' 
           className='calendly-link' 
-          aria-label='Schedul'
+          aria-label='Schedule'
         >
           <Button variant="contained" size='small' style={{width: '6rem', backgroundColor: 'black', marginTop: '-20px'}}>
             Schedule
           </Button>
         </a>
         </Grid>
-        
-      {/* </section> */}
       </Grid>
   );
 };

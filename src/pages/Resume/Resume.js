11f73de2ff8da4f18eec6a32637e8a14c54d1d81
Kevin Long <98487770+KLong75@users.noreply.github.com>
2023-04-21 18:30:34 -0500

    update resume

--- src/pages/Resume/Resume.js
+++ src/pages/Resume/Resume.js
@@ -9,7 +9,7 @@ import Education from "../../components/Education";
 import Grid from "@mui/material/Unstable_Grid2/Grid2";
 
 // import pdf
-import pdf from "../../assets/resume/Kevin_Long_Full_Stack_Software_Engineer-Resume.pdf";
+import pdf from "../../assets/resume/Kevin_Long_Full_Stack_Web_Developer-Resume.pdf";
 
 // import images
 import shtDesktop from "../../assets/images/projectImages/shtDesktop.png";
@@ -853,8 +853,6 @@ const Resume = () => {
                           <li>{jobs[3].keyAccomplishments[0]}</li>
                           <br />
                           <li>{jobs[3].keyAccomplishments[1]}</li>
-                          <br />
-                          <li>{jobs[3].keyAccomplishments[2]}</li>
                         </ul>
                       </DisplayBox>
                     </Grid>
